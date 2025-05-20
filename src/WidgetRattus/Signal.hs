89bc8f6f49b636be559d12561f76da1c1e2024f8
Philip Flyvholm <philipflyvholm@gmail.com>
2025-05-20 09:45:59 +0200

    Removed C from mkWidgetNode

--- src/WidgetRattus/Signal.hs
+++ src/WidgetRattus/Signal.hs
@@ -426,8 +426,7 @@ instance Continuous a => Continuous (Sig a) where
     progressInternal inp (x ::: xs@(Delay cl _)) = 
         if inputInClock inp cl then (adv' xs inp)
         else progressInternal inp x ::: xs
-    progressAndNext inp (x ::: xs@(Delay cl _)) = 
-        let _= unsafePerformIO (print "hallo") in
+    progressAndNext inp (x ::: xs@(Delay cl _)) =
         if inputInClock inp cl then let n = adv' xs inp in (n, nextProgress n)
         else let (n , cl') = progressAndNext inp x in (n ::: xs , cl `clockUnion` cl')
     nextProgress (x ::: (Delay cl _)) = nextProgress x `clockUnion` cl
